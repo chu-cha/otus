8741637e0727d99b95fba81bd3437cd94c0b1691
chu-cha <69756671+chu-cha@users.noreply.github.com>
2025-01-08 23:59:15 +0300

    Update test_version.cpp

--- hw1/test_version.cpp
+++ hw1/test_version.cpp
@@ -16,7 +16,6 @@ TEST(HelloTest, BasicAssertions) {
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version() = 0);
 	BOOST_CHECK(version() > 0);
 }
 
