96bb0945eca4daf26ca8ba3e44f8ddd0a4ddcf55
chu-cha <69756671+chu-cha@users.noreply.github.com>
2025-01-09 00:31:37 +0300

    Update test_version.cpp

--- hw1/test_version.cpp
+++ hw1/test_version.cpp
@@ -1,7 +1,16 @@
 #include "lib.h"
 
+#if USE_GTEST
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_GT(version(), 0);
+}
+#endif
 
 #define BOOST_TEST_MODULE test_version
+
+#if USE_BOOST_TEST
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_version)
