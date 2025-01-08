2199340a77297347f374c441dc54c468dc50087a
tk <tkardashova@thor3dscanner.com>
2025-01-08 23:43:43 +0300

    add 2 test types selection

--- hw1/test_version.cpp
+++ hw1/test_version.cpp
@@ -1,7 +1,24 @@
-#include <gtest/gtest.h>
-
 #include "lib.h"
+
+#ifdef USE_GTEST
+#include <gtest/gtest.h>
 
 TEST(HelloTest, BasicAssertions) {
   EXPECT_GT(version(), 0);
 }
+#endif
+
+#ifdef USE_GTEST
+
+#define BOOST_TEST_MODULE test_version
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version) {
+	BOOST_CHECK(version() = 0);
+	BOOST_CHECK(version() > 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+#endif
